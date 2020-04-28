d0f7a75d29fca97c03686cfb6bc4aed82f3b2f7c
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-04-28 22:26:16 +0530

    rm cache

--- services/fetch.py
+++
@@ -1,9 +0,0 @@
-import requests
-def get(url):
-    try:
-        response = requests.get(url)
-        # print( f"Request returned {response.status_code} : '{response.reason}'" )
-    except requests.HTTPError:
-        # print(response.status_code, response.reason)
-        raise
-    return response
