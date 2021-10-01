e4a3831f397627f88b3708d633ad54ee8250a8ba
Anujeet Chatterjee <anujeetc@vmware.com>
2021-10-01 21:40:33 +0530

    cleanup

--- services/country_wise_confirmed_recovered_and_deaths.py
+++ services/country_wise_confirmed_recovered_and_deaths.py
@@ -7,19 +7,9 @@ pd.options.mode.chained_assignment = None
 file_loc = ''  # deploy
 
 gov_data = pd.read_csv(file_loc + './data/covid_19_india.csv')
-'''
-c = 0
-for row in gov_data.index:
-  if gov_data['Date'][row] == '2020-04-13':
-    c += 1
-    if c == 2:
-      gov_data['Date'][row] = "2020-04-14"
-      c=0
-'''
 
 # for govt data
 gov_data['Date'] = pd.to_datetime(gov_data['Date'],dayfirst=True)
-# confirmed_death_recovered = gov_data.groupby('Date')['Date', 'Total Confirmed cases','Cured/Discharged/Migrated', 'Death'].sum().reset_index()
 gov_data = gov_data.rename(columns={"Confirmed": "Total Confirmed cases", 'Cured': "Cured/Discharged/Migrated", "State/UnionTerritory" : "Name of State / UT", "Deaths":"Death"})
 confirmed_death_recovered = gov_data.groupby('Date')['Date', 'Total Confirmed cases','Cured/Discharged/Migrated', 'Death'].sum().reset_index()
 c = 0
@@ -40,20 +30,15 @@ confirmed_death_recovered['tot_active'] = confirmed_death_recovered['Total Confi
 
 
 
-# diagnosed = pd.Series(confirmed_death_recovered['Date']).tolist()
-# diagnosed_date = [datetime.strptime(x, "%Y-%m-%d %H:%M:%S") for x in diagnosed]
 dates = pd.Series(confirmed_death_recovered['Date']).tolist()
 day_wise_encountered = pd.Series(confirmed_death_recovered['day_conf']).tolist()
 day_wise_confirmed_overall = pd.Series( confirmed_death_recovered['Total Confirmed cases']).tolist()
 
 
 confirmed_death_recovered['tot_active'] = confirmed_death_recovered['Total Confirmed cases'] - confirmed_death_recovered['Death'] - confirmed_death_recovered['Cured/Discharged/Migrated']
-# confirmed_death_recovered[['tot_active']]
-#print(confirmed_death_recovered["Cured/Discharged/Migrated"])
 recovery_daywise = pd.Series(confirmed_death_recovered['day_rec']).tolist()
 recovery_cumulative = pd.Series(confirmed_death_recovered['Cured/Discharged/Migrated']).tolist()
 dates = pd.Series(confirmed_death_recovered['Date']).tolist()
-#recovery_dates = [datetime.strptime(x, "%Y-%m-%d") for x in dates]
 
 death_cumulative = pd.Series(confirmed_death_recovered['Death']).tolist()
 death_day_wise = pd.Series(confirmed_death_recovered['day_death']).tolist()
