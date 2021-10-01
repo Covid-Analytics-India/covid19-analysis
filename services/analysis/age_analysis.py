78cc9c0a34ba1654265e369324238e8b9fb805a7
Anujeet Chatterjee <anujeetc@vmware.com>
2021-10-01 21:42:22 +0530

    double checking analysis function

--- services/analysis/age_analysis.py
+++ services/analysis/age_analysis.py
@@ -1,18 +1,15 @@
 from services.processes import raw_data
 import pandas as pd
-file_loc = ''  # deploy
-#file_loc = '../.' # production
+file_loc = '' 
 
-'''
-# Development
-from services.processes import getData, Data
-raw_data = getData(Data.raw_data)
-# print(raw_data)
-# -------------
-'''
 raw_data = pd.read_csv(file_loc + './data/raw_data.csv', dtype=object)
 clean_age = {'Age' : raw_data['Age Bracket']}
 clean_age = pd.DataFrame(clean_age)
+
+clean_age = pd.DataFrame(clean_age)
+clean_age = clean_age[clean_age['Age'] != '28-35']
+clean_age['Age'] = clean_age['Age'].astype(float)
+clean_age['Age'].dropna(inplace=True)
 
 clean_age = pd.DataFrame(clean_age)
 clean_age = clean_age[clean_age['Age'] != '28-35']
